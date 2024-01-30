2fc3f8286fc5c4994a6a0fc521595d58f25ee6cd
hvgochr <charrade.hugo@gmail.com>
2024-01-30 16:18:32 +0100

    Add random recipe button + css

--- src/views/AllRecipes.vue
+++ src/views/AllRecipes.vue
@@ -2,7 +2,11 @@
 import { ref, type Ref, onMounted } from 'vue'
 import type { Recipe } from '@/types'
 import BoiteRecipe from '@/components/BoiteRecipe.vue'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+const nbRecipes: Ref<number> = ref(0)
 const recipes: Ref<Recipe[]> = ref([
   {
     title: 'chargement',
@@ -24,21 +28,44 @@ const recipes: Ref<Recipe[]> = ref([
   }
 ])
 
-onMounted(() => {
-  fetch('http://127.0.0.1:8000/api/recipes')
-    .then((reponsehttp) => reponsehttp.json())
-    .then((reponseJSON) => {
-      recipes.value = reponseJSON['hydra:member']
-    })
+onMounted(async () => {
+  const response = await fetch('http://127.0.0.1:8000/api/recipes')
+  const reponseJSON = await response.json()
+  recipes.value = reponseJSON['hydra:member']
+  nbRecipes.value = recipes.value.length
 })
+
+function randomRecipe(): void {
+  const random = Math.floor(Math.random() * nbRecipes.value)
+  const recipe = recipes.value[random]
+  router.push({ name: 'singleRecipe', params: { id: recipe.id } })
+}
 </script>
 
 <template>
+  <div class="random-container">
+    <button class="random-button" @click="randomRecipe()">Recette aléatoire</button>
+  </div>
   <div class="all-recipe">
     <BoiteRecipe v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" :id="recipe.id" />
   </div>
 </template>
 <style scoped>
+.random-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 50px;
+}
+.random-button {
+  padding: 10px;
+  border-radius: 10px;
+  background-color: var(--dark-orange);
+  color: var(--font-color);
+  font-weight: 500;
+  font-size: 16px;
+}
 .all-recipe {
   display: grid;
   grid-gap: 20px;
