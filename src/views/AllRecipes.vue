e44a3b3ca2a1282c75535a34bfcd1b9d4244ea63
hvgochr <charrade.hugo@gmail.com>
2024-01-30 16:57:31 +0100

    Add filter by ingredient + random recipe

--- src/views/AllRecipes.vue
+++ src/views/AllRecipes.vue
@@ -7,6 +7,7 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 const nbRecipes: Ref<number> = ref(0)
+const ingredientNames: Ref<string[]> = ref(['chargement'])
 const recipes: Ref<Recipe[]> = ref([
   {
     title: 'chargement',
@@ -18,7 +19,7 @@ const recipes: Ref<Recipe[]> = ref([
     },
     preparation: ['chargement'],
     categoryNames: ['chargement'],
-    compositionsData: [
+    ingredients: [
       {
         ingredientName: 'chargement',
         quantity: 0,
@@ -27,13 +28,38 @@ const recipes: Ref<Recipe[]> = ref([
     ]
   }
 ])
+const filteredRecipes: Ref<Recipe[]> = ref(recipes.value)
+const selectedIngredient: Ref<string> = ref('')
 
 onMounted(async () => {
   const response = await fetch('http://127.0.0.1:8000/api/recipes')
   const reponseJSON = await response.json()
   recipes.value = reponseJSON['hydra:member']
   nbRecipes.value = recipes.value.length
+  recipes.value.forEach((recipe) => {
+    if (recipe.ingredients && Array.isArray(recipe.ingredients)) {
+      recipe.ingredients.forEach((ingredient) => {
+        if (!ingredientNames.value.includes(ingredient.ingredientName)) {
+          ingredientNames.value.push(ingredient.ingredientName)
+        }
+      })
+    }
+  })
+  ingredientNames.value.sort()
+  filteredRecipes.value = recipes.value
 })
+
+function filterRecipes() {
+  if (selectedIngredient.value === '') {
+    filteredRecipes.value = recipes.value
+  } else {
+    filteredRecipes.value = recipes.value.filter((recipe) =>
+      recipe.ingredients.some(
+        (ingredient) => ingredient.ingredientName === selectedIngredient.value
+      )
+    )
+  }
+}
 
 function randomRecipe(): void {
   const random = Math.floor(Math.random() * nbRecipes.value)
@@ -43,20 +69,52 @@ function randomRecipe(): void {
 </script>
 
 <template>
-  <div class="random-container">
-    <button class="random-button" @click="randomRecipe()">Recette aléatoire</button>
+  <div class="top-container">
+    <div class="filter-container">
+      <p class="filter-text">Filtrer par:</p>
+      <select v-model="selectedIngredient" @change="filterRecipes">
+        <option value="">Tous les ingrédients</option>
+        <option v-for="ingredient in ingredientNames" :key="ingredient" :value="ingredient">
+          {{ ingredient }}
+        </option>
+      </select>
+    </div>
+    <div class="random-container">
+      <button class="random-button" @click="randomRecipe()">Recette aléatoire</button>
+    </div>
   </div>
   <div class="all-recipe">
-    <BoiteRecipe v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" :id="recipe.id" />
+    <BoiteRecipe
+      v-for="recipe in filteredRecipes"
+      :key="recipe.id"
+      :recipe="recipe"
+      :id="recipe.id"
+    />
   </div>
 </template>
 <style scoped>
+.top-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  justify-content: space-around;
+}
+.filter-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 70px;
+}
+.filter-text {
+  margin-right: 10px;
+}
 .random-container {
   display: flex;
   justify-content: center;
   margin-top: 20px;
   margin-bottom: 20px;
-  margin-left: 50px;
 }
 .random-button {
   padding: 10px;
