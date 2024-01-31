571f121b92c7bf802e92f83cf079bac871ee76a5
hvgochr <charrade.hugo@gmail.com>
2024-01-31 12:09:51 +0100

    Fix errors

--- src/views/SingleUser.vue
+++ src/views/SingleUser.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import type { User, Recipe } from '@/types'
 import { useRouter } from 'vue-router'
 import { ref, type Ref, onMounted } from 'vue'
 import BoiteUser from '@/components/BoiteUser.vue'
@@ -7,7 +6,7 @@ import BoiteUser from '@/components/BoiteUser.vue'
 const router = useRouter()
 const id = router.currentRoute.value.params.id
 
-const user: Ref<User> = ref({
+const user: Ref<any> = ref({
   id: 0,
   login: 'string',
   status: 'string',
@@ -69,24 +68,26 @@ const user: Ref<User> = ref({
   coverImageName: 'string'
 })
 
-const recipes: Ref<Recipe[]> = ref({
-  title: 'chargement',
-  description: 'chargementM',
-  recipeDetails: {
-    difficulty: 0,
-    preparationTime: 0,
-    nbPersons: 0
-  },
-  preparation: ['chargement'],
-  categoryNames: ['chargement'],
-  compositionsData: [
-    {
-      ingredientName: 'chargement',
-      quantity: 0,
-      unit: 'chargement'
-    }
-  ]
-})
+const recipes: Ref<any[]> = ref([
+  {
+    title: 'chargement',
+    description: 'chargementM',
+    recipeDetails: {
+      difficulty: 0,
+      preparationTime: 0,
+      nbPersons: 0
+    },
+    preparation: ['chargement'],
+    categoryNames: ['chargement'],
+    compositionsData: [
+      {
+        ingredientName: 'chargement',
+        quantity: 0,
+        unit: 'chargement'
+      }
+    ]
+  }
+])
 
 onMounted(async () => {
   await fetch(encodeURI(String('http://127.0.0.1:8000/api/users/' + id)))
